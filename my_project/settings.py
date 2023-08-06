5ba56ecab88c3a5c8a43b1a3319c74c644fb0faf
Arohan Paul <arohan.paul@gmail.com>
2023-08-06 19:43:33 +0530

    third commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-v5reuq=xn7yi!#m_xh2#7p_$gbbvy@n&crpkz4x5s=^%t+4tuq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
